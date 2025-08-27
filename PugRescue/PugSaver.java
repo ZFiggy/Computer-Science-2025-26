31d933cc10ae889ee95aaa7d9dd4de9528b21e87
ZFiggy <zfiglin1@hwemail.com>
2025-08-27 13:32:03 -0700

    End of class August 27

--- PugRescue/PugSaver.java
+++ PugRescue/PugSaver.java
@@ -7,13 +7,22 @@ public class PugSaver {
 	public static void rescuePugs(ArrayList<Dog> list) {
 		ArrayList<Dog> goodBoys = new ArrayList<Dog>();
 		ArrayList<Dog> badBoys = new ArrayList<Dog>();
+		int end = list.size() - 1;
 		for (int i = 0; i < list.size(); i++) {
+			if (end == i) {
+					break;
+				}
 			if (list.get(i).getBreed().toLowerCase().contains("golden")) {
 				goodBoys.add(list.get(i));
-				list.remove(i);
+				list.set(i, list.get(end));
+				list.set(end, goodBoys.get(goodBoys.size() - 1));
+				end--;
 				i--;
+			} else {
+				badBoys.add(list.get(i));
 			}
 		}
+		list = badBoys;
 		list.addAll(goodBoys);
 	}
 }
---
+++ PugRescue/PugSaverTester.java
@@ -0,0 +1,7 @@
+import java.util.ArrayList;
+
+public class PugSaverTester {
+    public static void main(String[] args) {
+        ArrayList<Dog> testList = new ArrayList<Dog>();
+    }
+}
