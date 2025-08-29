9531da11b53a40b363c039f3854e7f12df9fa863
ZFiggy <zfiglin1@hwemail.com>
2025-08-29 11:15:59 -0700

    Additions for August 29

--- PugRescue/PugSaver.java
+++ PugRescue/PugSaver.java
@@ -5,24 +5,46 @@ public class PugSaver {
 
 	//Moves every dog whose breed is "Pug" in the list to the back of the list
 	public static void rescuePugs(ArrayList<Dog> list) {
-		ArrayList<Dog> goodBoys = new ArrayList<Dog>();
-		ArrayList<Dog> badBoys = new ArrayList<Dog>();
-		int end = list.size() - 1;
+		int goldenCounter = 0;
 		for (int i = 0; i < list.size(); i++) {
-			if (end == i) {
+			String breed = list.get(i).getBreed();
+			if (breed.toLowerCase().contains("golden")) {
+				Dog temp = list.get(i);
+
+				for (int j = list.size() - goldenCounter; j > 0; j--) {
+					if (i >= j) {
+						break;
+					}
+					if (!list.get(j).getBreed().toLowerCase().contains("golden")) {
+						list.set(i, list.get(j));
+						list.set(j, temp);
+						goldenCounter++;
+					}
+				}
+				if (goldenCounter + i > list.size()) {
 					break;
 				}
-			if (list.get(i).getBreed().toLowerCase().contains("golden")) {
-				goodBoys.add(list.get(i));
-				list.set(i, list.get(end));
-				list.set(end, goodBoys.get(goodBoys.size() - 1));
-				end--;
-				i--;
-			} else {
-				badBoys.add(list.get(i));
 			}
+			System.out.println(list.get(0).getBreed());
 		}
-		list = badBoys;
-		list.addAll(goodBoys);
+		// ArrayList<Dog> goodBoys = new ArrayList<Dog>();
+		// ArrayList<Dog> badBoys = new ArrayList<Dog>();
+		// int end = list.size() - 1;
+		// for (int i = 0; i < list.size(); i++) {
+		// 	if (end == i) {
+		// 			break;
+		// 		}
+		// 	if (list.get(i).getBreed().toLowerCase().contains("golden")) {
+		// 		goodBoys.add(list.get(i));
+		// 		list.set(i, list.get(end));
+		// 		list.set(end, goodBoys.get(goodBoys.size() - 1));
+		// 		end--;
+		// 		i--;
+		// 	} else {
+		// 		badBoys.add(list.get(i));
+		// 	}
+		// }
+		// list = badBoys;
+		// list.addAll(goodBoys);
 	}
 }
--- PugRescue/PugSaverTester.java
+++ PugRescue/PugSaverTester.java
@@ -3,5 +3,8 @@ import java.util.ArrayList;
 public class PugSaverTester {
     public static void main(String[] args) {
         ArrayList<Dog> testList = new ArrayList<Dog>();
+        testList.add(new Dog("Ziggy", "Golden Doodle"));
+        testList.add(new Dog("Charlie"));
+        PugSaver.rescuePugs(testList);
     }
 }
