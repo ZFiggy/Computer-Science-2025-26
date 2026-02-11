b14009cb6c599e8487acc0bfcc693318e01eea00
ZFiggy <zfiglin1@hwemail.com>
2026-02-11 11:24:34 -0800

    Nothing works

--- HeapPQ/HeapPQ.java
+++ HeapPQ/HeapPQ.java
@@ -1,45 +1,38 @@
+import java.util.EmptyStackException;
 
 public class HeapPQ<E extends Comparable<E>> implements MyPriorityQueue<E> {
 
 	private E[] heap;
 	private int objectCount;
 
-	public HeapPQ()
-	{
-		this.heap = (E[])new Comparable[3];
+	public HeapPQ() {
+		this.heap = (E[]) new Comparable[3];
 		this.objectCount = 0;
 	}
 
-	//Returns the number of elements in the priority queue
-	public int size()
-	{
+	// Returns the number of elements in the priority queue
+	public int size() {
 		return objectCount;
 	}
 
-	//DO NOT CHANGE MY JANKY TOSTRING!!!!!
-	public String toString()
-	{
+	// DO NOT CHANGE MY JANKY TOSTRING!!!!!
+	public String toString() {
 		StringBuffer stringbuf = new StringBuffer("[");
-		for (int i = 0; i < objectCount; i++)
-		{
+		for (int i = 0; i < objectCount; i++) {
 			stringbuf.append(heap[i]);
 			if (i < objectCount - 1)
 				stringbuf.append(", ");
 		}
 		stringbuf.append("]\nor alternatively,\n");
 
-		for(int rowLength = 1, j = 0; j < objectCount; rowLength *= 2)
-		{
-			for (int i = 0; i < rowLength && j < objectCount; i++, j++)
-			{
+		for (int rowLength = 1, j = 0; j < objectCount; rowLength *= 2) {
+			for (int i = 0; i < rowLength && j < objectCount; i++, j++) {
 				stringbuf.append(heap[j] + " ");
 			}
 			stringbuf.append("\n");
-			if (j < objectCount)
-			{
-				for (int i = 0; i < Math.min(objectCount - j, rowLength*2); i++)
-				{
-					if (i%2 == 0)
+			if (j < objectCount) {
+				for (int i = 0; i < Math.min(objectCount - j, rowLength * 2); i++) {
+					if (i % 2 == 0)
 						stringbuf.append("/");
 					else
 						stringbuf.append("\\ ");
@@ -50,19 +43,17 @@ public class HeapPQ<E extends Comparable<E>> implements MyPriorityQueue<E> {
 		return stringbuf.toString();
 	}
 
-	//Doubles the size of the heap array
-	private void increaseCapacity()
-	{
-		E[] newHeap = (E[])new Comparable[heap.length * 2];
+	// Doubles the size of the heap array
+	private void increaseCapacity() {
+		E[] newHeap = (E[]) new Comparable[heap.length * 2];
 		for (int i = 0; i < heap.length; i++) {
 			newHeap[i] = heap[i];
 		}
 		heap = newHeap;
 	}
 
-	//Returns the index of the "parent" of index i
-	private int parent(int i)
-	{
+	// Returns the index of the "parent" of index i
+	private int parent(int i) {
 		if (i >= objectCount) {
 			throw new IllegalArgumentException();
 		}
@@ -71,9 +62,9 @@ public class HeapPQ<E extends Comparable<E>> implements MyPriorityQueue<E> {
 		}
 		return (i - 1) / 2;
 	}
-	//Returns the index of the *smaller child* of index i
-	private int smallerChild(int i)
-	{
+
+	// Returns the index of the *smaller child* of index i
+	private int smallerChild(int i) {
 		if (i >= objectCount) {
 			throw new IllegalArgumentException();
 		}
@@ -96,9 +87,8 @@ public class HeapPQ<E extends Comparable<E>> implements MyPriorityQueue<E> {
 		return 2 * i + 1;
 	}
 
-	//Swaps the contents of indices i and j
-	private void swap(int i, int j)
-	{
+	// Swaps the contents of indices i and j
+	private void swap(int i, int j) {
 		if (i >= objectCount || j >= objectCount || i < 0 || j < 0) {
 			throw new IllegalArgumentException();
 		}
@@ -108,8 +98,7 @@ public class HeapPQ<E extends Comparable<E>> implements MyPriorityQueue<E> {
 	}
 
 	// Bubbles the element at index i upwards until the heap properties hold again.
-	private void bubbleUp(int i)
-	{
+	private void bubbleUp(int i) {
 		if (i < 0 || i >= objectCount) {
 			throw new IllegalArgumentException();
 		}
@@ -125,9 +114,9 @@ public class HeapPQ<E extends Comparable<E>> implements MyPriorityQueue<E> {
 		}
 	}
 
-	// Bubbles the element at index i downwards until the heap properties hold again.
-	private void bubbleDown(int i)
-	{
+	// Bubbles the element at index i downwards until the heap properties hold
+	// again.
+	private void bubbleDown(int i) {
 		if (i < 0 || i >= objectCount) {
 			throw new IllegalArgumentException();
 		}
@@ -141,6 +130,47 @@ public class HeapPQ<E extends Comparable<E>> implements MyPriorityQueue<E> {
 			}
 		} catch (Exception e) {
 			return;
+		}
+	}
+
+	// Adds obj to the Priority Queue
+	public void add(E obj) {
+		if (objectCount == heap.length) {
+			increaseCapacity();
+		}
+		heap[objectCount] = obj;
+		bubbleUp(objectCount);
+		objectCount++;
+	}
+
+	// Removes and returns the MINIMUM element from the Priority Queue
+	public E removeMin() {
+		if (isEmpty()) {
+			throw new EmptyStackException();
+		} else {
+			E remove = heap[0];
+			heap[0] = heap[objectCount - 1];
+			bubbleDown(0);
+			objectCount--;
+			return remove;
+		}
+	}
+
+	// Returns the MINIMUM element from the Priority Queue without removing it
+	public E peek() {
+		if (!isEmpty()) {
+			return heap[0];
+		} else {
+			throw new EmptyStackException();
+		}
+	}
+
+	// Returns true if the priority queue is empty
+	public boolean isEmpty() {
+		if (objectCount == 0) {
+			return true;
+		} else {
+			return false;
 		}
 	}
 
