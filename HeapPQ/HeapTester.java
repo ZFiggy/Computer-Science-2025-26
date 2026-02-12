37f5e31763278af1d1f142569e4cfe7779a67360
ZFiggy <zfiglin1@hwemail.com>
2026-02-12 13:11:22 -0800

    i did it

---
+++ HeapPQ/HeapTester.java
@@ -0,0 +1,16 @@
+public class HeapTester {
+    public static void main(String[] args) {
+        HeapPQ heap = new HeapPQ<>();
+        heap.add(10);
+		heap.add( 6);
+		heap.add(11);
+		heap.add( 8);
+		heap.add( 9);
+		heap.add( 5);
+		heap.add( 7);
+        System.out.println(heap.peek());
+        System.out.println(heap.removeMin());
+        System.out.println(heap.toString());
+        System.out.println(heap.isEmpty());
+    }
+}
